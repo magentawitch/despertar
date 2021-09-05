96080bd941cb1519d40ca0560859c84b838c0ac1
Melissa Kuris <melissakuris@hotmail.com>
2021-09-05 12:58:46 -0300

    Scaffold godot project

---
+++ Prefabs/Boton.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+export var pantallaDestino = "res://XXXXX.tscn"
+export var texturaNormal : Texture = null
+export var texturaHover : Texture = null
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_mouse_entered():
+	print ("Mouse entro a boton")
+	if texturaHover:
+		$Sprite.set_texture(texturaHover)
+	else:
+		 $Sprite.modulate = Color(0, 1, 1, 1)
+
+
+func _on_Area2D_mouse_exited():
+	print ("Mouse salio del boton")
+	$Sprite.modulate = Color(1, 1, 1, 1)
+	if texturaNormal:
+		$Sprite.set_texture(texturaNormal)
+
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_pressed():
+		get_tree().change_scene(pantallaDestino)
