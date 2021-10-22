60669dec660f882ae573e893310c28eaab10b380
Nikita Zdanovitch <nzdanovitch@gmail.com>
2021-10-22 00:39:53 -0300

    Agregado de cues de sonidos de interacción con objetos interactivos.

--- utileria/ObjetoInteractivo.gd
+++ utileria/ObjetoInteractivo.gd
@@ -1,11 +1,30 @@
 extends TextureButton
 class_name ObjectoInteractivo, "res://assets/iconos/joystick.png"
 
-onready var forma_de_cursor_guardada = mouse_default_cursor_shape
+export var on_pressed_cue: AudioStream
+export var on_mouse_entered_cue: AudioStream
+export var on_mouse_exited_cue: AudioStream
+
+onready var _forma_de_cursor_guardada = mouse_default_cursor_shape
+var player: AudioStreamPlayer
+
+func _ready():
+	player = AudioStreamPlayer.new()
+	add_child(player)
+	connect("pressed", self, "play_sound", [on_pressed_cue])
+	connect("mouse_entered", self, "play_sound", [on_mouse_entered_cue])
+	connect("mouse_exited", self, "play_sound", [on_mouse_exited_cue])
+	
+func play_sound(auidio_stream: AudioStream):
+	if player.playing:
+		player.stop()
+	player.stream = auidio_stream
+	player.play()
+	
 
 func rehabilitar_interaccion():
 	disabled = false
-	mouse_default_cursor_shape = forma_de_cursor_guardada
+	mouse_default_cursor_shape = _forma_de_cursor_guardada
 
 func deshabilitar_interaccion():
 	disabled = true
