9c91be0d32cfdd2059b7332521ff2ea3c7d254d6
Nikita Zdanovitch <nzdanovitch@gmail.com>
2021-10-22 01:18:05 -0300

    Jade que se mueve cuando se mueve la pantalla.

---
+++ personajes/Personaje.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+class_name Personaje, "res://assets/iconos/singleplayer.png"
+
+export var sprite_path: NodePath
+export var velocidad: float = 450.0
+var facing = "right"
+
+func sprite() -> Sprite:
+	return get_node(sprite_path) as Sprite
+
+func _process(delta: float) -> void:
+	var desplazamiento = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		desplazamiento.x += 1
+	if Input.is_action_pressed("ui_left"):
+		desplazamiento.x -= 1
+	if desplazamiento.length() > 0:
+		desplazamiento = desplazamiento.normalized() * velocidad * delta
+		
+		if facing == "left" and desplazamiento.x > 0:
+			facing = "right"
+			sprite().scale.x = -sprite().scale.x
+			
+		if facing == "right" and desplazamiento.x < 0:
+			facing = "left"
+			sprite().scale.x = -sprite().scale.x
+		
+	position += desplazamiento
+	
