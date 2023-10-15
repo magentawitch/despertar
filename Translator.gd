a503cf94da72773fb3b9cc84cfad47cf3e175c67
Jbat1Jumper <nzdanovitch@gmail.com>
2023-10-15 01:10:13 -0300

    Comments around translator.

--- Translator.gd
+++ Translator.gd
@@ -6,18 +6,17 @@ var current_lang = "ENGLISH"
 func set_current_lang(lang):
 	current_lang = lang
 
+# Busca una traducción en el TSV con el mismo ID.
+# En caso de que no exista o no sea un id, devuelve el mismo texto.
+# Ej:
+#  translate("hello_world") -> "Hola mundirijillo :)"
+#  translate("Cualquier otra string random") -> "Cualquier otra string random"
 func translate(translation_id: String) -> String:
 	for r in records:
 		if r["ID"] == translation_id and r.has(current_lang):
 			return r[current_lang]
 	push_error("Translation not found for: %s" % translation_id)
 	return translation_id
-
-
-func translate_tree(node: Node):
-	translate_node(node)
-	for child in node.get_children():
-		translate_tree(child)
 
 func translate_node(node: Node):
 	if node.is_in_group("already_translated"):
@@ -29,3 +28,9 @@ func translate_node(node: Node):
 		
 	if node is Button:
 		node.text = translate(node.text)
+
+# Traduce todo un arbol o una escena, yendo por cada nodo y traduciendo su contenido.
+func translate_tree(node: Node):
+	translate_node(node)
+	for child in node.get_children():
+		translate_tree(child)
